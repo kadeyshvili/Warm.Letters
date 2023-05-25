6b9a918dd220d9b7f3c2c7e8b11e2157eb61576f
vikahse <57991510+vikahse@users.noreply.github.com>
2023-05-25 18:10:06 +0300

    Update Client.java

--- application/app/src/main/java/com/example/myapplication/Client.java
+++ application/app/src/main/java/com/example/myapplication/Client.java
@@ -53,7 +53,7 @@ public class Client extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        try (Socket socket = new Socket("192.168.50.168", 800)) {
+        try (Socket socket = new Socket("0.tcp.eu.ngrok.io", 15864)) {
 
             dataInputStream = new DataInputStream(
                     socket.getInputStream());
